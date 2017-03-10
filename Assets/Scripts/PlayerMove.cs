e74dfc29853f511ecdf6f05ebdf3e3df25179de1
Teemu Kokkonen <teemu.kokkonen@koudata.fi>
2017-03-10 13:25:43 +0200

    Initial Commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMove : MonoBehaviour {
+    public float MoveSpeed = 10.0f;
+    Vector3 _moveVector = new Vector3();
+    Rigidbody _rb;
+	// Use this for initialization
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+        _rb.constraints = RigidbodyConstraints.FreezeRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _moveVector.x = Input.GetAxis("Horizontal");
+        _moveVector.z = Input.GetAxis("Vertical");
+
+        _rb.MovePosition(transform.position + _moveVector.normalized * Time.deltaTime * MoveSpeed);
+
+    }
+}
