fa64a6acc6ed4ad1e07339b35cbd9aaac162b314
Teemu Kokkonen <teemu.kokkonen@koudata.fi>
2017-03-23 09:28:41 +0200

    Attributes, punch script

---
+++ Assets/Scripts/CharacterAttributes.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAttributes : MonoBehaviour {
+    public int Level = 1;
+    public int XP = 0;
+    public int HP = 0;
+    public int MaxHP = 100;
+    public int Attack = 10;
+    public int Defence = 5;
+    public float AttackRange = 1.0f;
+
+    void Start()
+    {
+        HP = MaxHP;
+    }
+
+    public int TakeHit(CharacterAttributes attacker)
+    {
+        int damageTaken = 0;
+        damageTaken = attacker.Attack - Defence;
+        if(damageTaken < 0)
+        {
+            damageTaken = 0;
+        }
+        ApplyDamage(damageTaken);
+        return damageTaken;
+    }
+
+    public void ApplyDamage(int amount)
+    {
+        HP -= amount; // Sama kuin: HP = HP - amount;
+        if(HP <= 0)
+        {
+            // TODO: Die.
+            Debug.Log(name + " IS DEAD!");
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Punch.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterAttributes))]
+public class Punch : MonoBehaviour {
+    public KeyCode AttackKey = KeyCode.Mouse0;  // Hyökkäyspainike
+    public float AttackInterval = 1.0f;         // Hyökkäysten väli sekunneissa
+    float _lastAttack = -1.0f;
+    CharacterAttributes _myAttributes;          
+    bool _isPlayer = false;
+
+	// Use this for initialization
+	void Start () {
+        _myAttributes = GetComponent<CharacterAttributes>();
+        _isPlayer = transform.CompareTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+         * 
+         * */         
+         
+        if(_isPlayer && Input.GetKeyDown(AttackKey))
+        {
+            Attack();
+        }
+
+        /* If the attached character is not the player,
+         * attack every AttackInterval seconds.
+         * */
+        if (!_isPlayer)
+        {
+            if(Time.time - _lastAttack > AttackInterval)
+            {
+                Attack();
+            }
+        }
+
+
+	}
+
+    void Attack()
+    {
+        _lastAttack = Time.time;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, _myAttributes.AttackRange))
+        {
+            CharacterAttributes defenderAttr = hit.transform.GetComponent<CharacterAttributes>();
+            if (defenderAttr)
+            {
+                defenderAttr.TakeHit(_myAttributes);
+            }
+        }
+    }
+}
